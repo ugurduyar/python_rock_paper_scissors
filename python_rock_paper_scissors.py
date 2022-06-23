ad6e32929f2a7c4b2cea8edc0f25a3b88b8fd0d9
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 17:52:58 +0300

    added point calculator

--- python_rock_paper_scissors.py
+++ python_rock_paper_scissors.py
@@ -1,5 +1,6 @@
 import random
 user_pick = input("Type ok if you are ready ")
+user_points = 0
 while user_pick != "quit":
     user_pick = input("Rock, Paper or Scissors? ")
     rps_list = ["rock","paper","scissors"]
@@ -13,13 +14,20 @@ while user_pick != "quit":
         print("Draw!")
     if user_pick == "scissors" and computer_pick == "paper":
         print("You Win!")
+        user_points += 1
     if user_pick == "rock" and computer_pick == "scissors":
         print("You Win!")
+        user_points += 1
     if user_pick == "paper" and computer_pick == "rock":
         print("You Win!")
+        user_points += 1
     if user_pick == "paper" and computer_pick == "scissors":
         print("You Lost!")
+        user_points -= 1
     if user_pick == "scissors" and computer_pick == "rock":
         print("You Lost!")
+        user_points -= 1
     if user_pick == "rock" and computer_pick == "paper":
         print("You Lost!")
+        user_points -= 1
+    print("You have {} points!".format(user_points))
