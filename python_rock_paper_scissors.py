734994b5038cb8c2d17999b816573cb3bd701a9c
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 17:49:35 +0300

    removed initial question

--- python_rock_paper_scissors.py
+++ python_rock_paper_scissors.py
@@ -1,13 +1,9 @@
 import random
-user_pick = input("Rock, Paper or Scissors? ")
-rps_list = ["rock","paper","scissors"]
-computer_pick = random.choice(rps_list)
-print(user_pick)
-print(computer_pick)
+user_pick = input("Type ok if you are ready ")
 while user_pick != "quit":
     user_pick = input("Rock, Paper or Scissors? ")
+    rps_list = ["rock","paper","scissors"]
     computer_pick = random.choice(rps_list)
-    print(user_pick)
     print(computer_pick)
     if user_pick == "rock" and computer_pick == "rock":
         print("Draw!")
@@ -16,7 +12,7 @@ while user_pick != "quit":
     if user_pick == "scissors" and computer_pick == "scissors":
         print("Draw!")
     if user_pick == "scissors" and computer_pick == "paper":
-        print("You Win")
+        print("You Win!")
     if user_pick == "rock" and computer_pick == "scissors":
         print("You Win!")
     if user_pick == "paper" and computer_pick == "rock":
